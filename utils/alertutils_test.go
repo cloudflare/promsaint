7859f8f5e86ad1506a4819d4778bcea1e59bb345
Kai Storbeck <kai@xs4all.nl>
2017-09-26 23:12:27 +0200

    Seeing multiple errors now in alertmanager

--- utils/alertutils_test.go
+++ utils/alertutils_test.go
@@ -32,7 +32,7 @@ var testAlert3 = &models.Alert{
 	Type:             "service",
 	Host:             "bar",
 	Service:          "foo",
-	Notify:           "hipchat-bar hipchat-cafe",
+	Notify:           "hipchat-cafe",
 	NotificationType: "PROBLEM",
 	State:            "CRITICAL",
 	Message:          "It's dead jim",
@@ -52,19 +52,23 @@ func TestKey(t *testing.T) {
 	if key == key3 {
 		t.Fatal("Hashes match for different object")
 	}
+
+	if got := Key(testAlert3); got != key2 {
+		t.Fatalf("Hashes should have matched. got %#v, want %#v", got, key2)
+	}
 }
 
 func TestMerge(t *testing.T) {
 	pAlert := models.InternalAlert{}
 	Merge(&pAlert, testAlert)
 	if got := pAlert.PrometheusAlert.Labels["notify"]; string(got) != testAlert.Notify {
-		t.Errorf("Merge failed: got %#v, expect %#v", got, testAlert.Notify)
+		t.Errorf("Merge failed: got %#v, want %#v", got, testAlert.Notify)
 	}
 
-	pAlert = models.InternalAlert{}
+	// now test merging a new field:
 	Merge(&pAlert, testAlert3)
-	if got := pAlert.PrometheusAlert.Labels["notify"]; string(got) != testAlert3.Notify {
-		t.Errorf("Merge failed: got %#v, expect %#v", got, testAlert3.Notify)
+	want := "hipchat-bar hipchat-cafe"
+	if got := pAlert.PrometheusAlert.Labels["notify"]; string(got) != want {
+		t.Errorf("Merge failed: got %#v, want %#v", got, want)
 	}
-
 }
